626683b65954cea8dc656b65d35cf4b5f1080cf9
River <oasisnile556@gmail.com>
2022-11-08 15:04:48 -0500

    3

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Layout from "./layout";
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Layout />} />
+      <Route path="*" element={<Layout />} />
     </Routes>
   );
 }
