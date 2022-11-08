3551b24cd9c85567ead38d2b9e8ab5ba0a7be41b
River <oasisnile556@gmail.com>
2022-11-07 19:42:05 -0500

    Navbar update, home and catalog link 90%

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,3 +1,19 @@
+import { Link } from "react-router-dom";
+
 export default function Navbar() {
-  return <h1>Hello</h1>;
+  return (
+    <div className="navbar">
+      <h3>POSITIVE RATE GEAR</h3>
+      <div className="d-flex position-absolute top-50 start-50 translate-middle align-middle">
+        <Link className="homeBtn">Home</Link>
+        <Link>Catalog</Link>
+      </div>
+
+      <div>
+        <i class="bi bi-search"></i>
+        <i class="bi bi-person"></i>
+        <i class="bi bi-bag"></i>
+      </div>
+    </div>
+  );
 }
