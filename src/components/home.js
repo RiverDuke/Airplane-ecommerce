5166a2f94d77f1095de5e0e781b9166b1af74104
River <oasisnile556@gmail.com>
2022-11-09 22:22:57 -0500

    logo and banner

--- src/components/home.js
+++ src/components/home.js
@@ -1,7 +1,14 @@
+import { Link } from "react-router-dom";
+
 export default function HomePage() {
   return (
     <div className="home">
-      <h1>Hello</h1>
+      <div className="home-logo image-sizing"></div>
+      <div className="text-center buy-banner">
+        <p>Your Aviators Are Here</p>
+        <p>Quality Aviator Sunglasses at a Fair Price</p>
+        <Link className="btn btn-secondary">ORDER NOW</Link>
+      </div>
     </div>
   );
 }
