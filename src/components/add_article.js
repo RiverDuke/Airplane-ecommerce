7b590fe869a1871a506418cc66e29dac821872f1
River <oasisnile556@gmail.com>
2022-12-21 19:36:44 -0500

    testing articles

--- src/components/add_article.js
+++ src/components/add_article.js
@@ -1,3 +1,59 @@
 export default function AddArticle() {
-  return <h1>Hello Article</h1>;
+  return (
+    <div>
+      Please use the sharing tools found via the share button at the top or side
+      of articles. Copying articles to share with others is a breach of FT.com
+      T&Cs and Copyright Policy. Email licensing@ft.com to buy additional
+      rights. Subscribers may share up to 10 or 20 articles per month using the
+      gift article service. More information can be found at
+      https://www.ft.com/tour.
+      https://www.ft.com/content/e9a2b2df-6e16-49d0-8ff1-4f48a225d6e7 Donald
+      Trump paid $1.8mn in federal income tax for the six years between 2015 and
+      2020, as he declared $53mn in net losses over the period spanning his 2016
+      campaign for the White House and the bulk of his term as US president. The
+      details of Trump’s tax payments came in a summary of his tax returns
+      released by the Democratic-led US House ways and means committee this week
+      after four years of legal wrangling. The panel is expected to publish full
+      redacted versions of Trump’s tax returns in the coming days. The summary,
+      which was compiled by Congress’s non-partisan Joint Committee on Taxation,
+      showed Trump declared no taxable income for 2015, 2016, 2017 and 2020,
+      years in which he racked up $82mn in combined losses. The two years for
+      which he reported taxable income were 2018 and 2019, with adjusted gross
+      income of nearly $29mn. The documents show the extent to which Trump
+      aggressively used tax deductions and reported losses in order to lower his
+      tax bill at the height of his political career and his public service. The
+      JCT said it had “no opinion” on whether the former president should have
+      paid more or less tax than he actually did during those years, but it
+      noted a series of items on Trump’s tax returns. Among them were interest
+      income from related-party loans made to some of Trump’s children,
+      including Ivanka Trump, Eric Trump and Donald Trump Jr. The JCT said these
+      raised “the question of whether the loans were bona fide arms lengths
+      transactions or whether the transfers were disguised gifts that could
+      trigger gift tax and a disallowance of interest deductions by the related
+      borrowers”. Like many real estate developers, Trump’s business is
+      structured through tax-exempt “pass-through” vehicles where ultimate tax
+      is paid on his personal returns submitted to the Internal Revenue Service.
+      The report noted the majority of the Trump vehicles in some years reported
+      “either no gross income (ie only expenses), or gross income and expenses
+      that entirely offset, raising the questions of whether these were valid
+      trade or business activities, or whether these schedules contained costs
+      derived from personal activities or hobbies.” Richard Neal, the Democratic
+      chair of the ways and means committee, has justified his pursuit and
+      release of Trump’s tax returns on the grounds that the IRS failed to
+      perform a mandatory audit of the ex-president’s tax returns while was in
+      the White House. “For four years, the committee has been reviewing how the
+      IRS enforces the federal tax laws against, and ensures compliance by, a
+      president,” he said on Tuesday night. “A president is no ordinary
+      taxpayer. They hold power and influence unlike any other American. And
+      with great power comes even greater responsibility,” he added. But
+      Republicans have lashed out at the publication of the tax returns as
+      politically motivated. “I am deeply concerned by recent erosions of
+      taxpayer confidentiality and the dangerous precedent today’s release sets,
+      which undermines confidence in both our tax laws and legislature,” Mike
+      Crapo, the top Republican on the Senate finance committee, said on
+      Wednesday.
+      <img src="https://www.ft.com/__origami/service/image/v2/images/raw/https%3A%2F%2Fd1e00ek4ebabms.cloudfront.net%2Fproduction%2F606e39ac-74b1-44a2-8224-94fe88411ac9.jpg?dpr=1&fit=scale-down&source=next&width=700"></img>
+      <p></p>
+    </div>
+  );
 }
